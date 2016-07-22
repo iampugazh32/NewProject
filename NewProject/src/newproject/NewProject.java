c6161e06364644cf953b1aa4d4283018aedabf71
iampugazh32 <iampugazhit@gmail.com>
2016-07-22 17:31:41 +0100

    to udate
    dddddd

--- NewProject/src/newproject/NewProject.java
+++ NewProject/src/newproject/NewProject.java
@@ -15,7 +15,7 @@ public class NewProject {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        System.out.println("HELLO WORLD");
+        System.out.println("HELLO WORLD");NewProject/src/newproject/NewProject.java
     }
     
 }
